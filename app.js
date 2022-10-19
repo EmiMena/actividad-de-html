44263892957cb3a26a4244e5a817425e1325a7fc
EmiMena <emilianomena2019@hotmail.com>
2022-10-19 19:19:13 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//IMPORTAR
+const express= require ('express');
+//PATH
+const path = require('path');
+// INSTANCIARLO
+const app = express();
+
+
+const publicFolderPath = path.resolve(__dirname, './public');
+//Defirnir la carpeta pública
+app.use(express.static(publicFolderPath));
+
+//Levantar el server
+const APP_PORT = 1234;
+app.listen(APP_PORT, () => {
+    console.log('Servidor funcionando en puerto' + APP_PORT)
+})
+
+//Definir rutas que devuelvan HTML
+app.get('/', (req, res) =>{
+    //Devolver un documento HTML
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/views/home.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
